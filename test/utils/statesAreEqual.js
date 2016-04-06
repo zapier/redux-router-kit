8304e4676b7670d5f5f4048c7609cd5f895ce060
Justin Deal <justin.deal@zapier.com>
2016-04-06 00:15:29 -0500

    fix and test statesAreEqual

---
+++ test/utils/statesAreEqual.js
@@ -0,0 +1,24 @@
+import test from 'ava';
+
+import 'babel-core/register';
+import statesAreEqual from 'redux-router-kit/src/utils/statesAreEqual';
+
+test('null equal null', t => {
+  t.true(statesAreEqual(null, null));
+});
+
+test('null not equal object', t => {
+  t.false(statesAreEqual(null, {}));
+});
+
+test('object not equal null', t => {
+  t.false(statesAreEqual({}, null));
+});
+
+test('object equals similar object', t => {
+  t.true(statesAreEqual({x: 1}, {x: 1}));
+});
+
+test('object not equal different object', t => {
+  t.false(statesAreEqual({x: 1}, {x: 2}));
+});
