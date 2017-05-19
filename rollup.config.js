edef89b2ef54e7b3dc0dc8ea89fd9000d95f0de1
Justin Deal <justin.deal@zapier.com>
2017-05-19 08:32:36 -0500

    use rollup to shrink build

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'src/index.js',
+  format: 'cjs',
+  external: [
+    'history/lib/createBrowserHistory',
+    'history/lib/createLocation',
+    'path-to-regexp',
+    'query-string',
+    'react',
+    'react-redux',
+    'url-parse'
+  ],
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [
+        ['es2015', {modules: false}],
+        ['stage-2'],
+        ['react']
+      ],
+      plugins: ['external-helpers']
+    }),
+  ],
+  dest: 'lib/index.js'
+};
